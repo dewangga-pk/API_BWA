c83bc29c4332940260c568ba601ef2ddb60d527e
dewangga-pk <pacetcommunity570@gmail.com>
2023-05-20 04:06:09 +0700

    service-payment getOrder

--- service-order-payment/routes/api.php
+++ service-order-payment/routes/api.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(OrderController::class)->group(function () {
     Route::post('/orders', 'create');
+    Route::get('/orders', 'index');
 });
